fb4106223a3294378ca3922dfe9ce044ca6ec0c9
grailedemperor <105563014+grailedemperor@users.noreply.github.com>
2022-07-14 12:13:33 -0700

    added for loop

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,67 @@
 // TODO: add game logic
+var questions = [ 
+    {
+        "title": "question1"
+        "options": ["opt1", "opt2", "opt3", "opt4"]
+        "answer": "opt1"
+    }
 
+    {
+        "title": "question2"
+        "options": ["opt1", "opt2", "opt3", "opt4"]
+        "answer": "opt3"
+    }
+
+    {
+        "title": "question3"
+        "options": ["opt1", "opt2", "opt3", "opt4"]
+        "answer": "opt4"
+    }
+
+    {
+        "title": "question4"
+        "options": ["opt1", "opt2", "opt3", "opt4"]
+        "answer": "opt2"
+    }
+]
+
+
+var globalIndex = 0;
 var startBtnEl = document .getElementById('start-btn');
+var timerInterval;
+var timerEl = document.getElementById('timer');
+var time  = 75;
+var CorrectWav = document.getElementById("CorrectWav");
+var inCorrectWav = document.getElementById("inCorrectWav");
 
-const timerEl = document.getElementById('timer');
-let time  = 75;
+var timerInterval = setInterval(timerFunc, 1000);
 
-const timerInterval = setInterval(timerFunc, 1000);
+function checkAnswer() {
+    let result;
+    if(option.onclick === answer){
+       result = 'Correct' + CorrectWav.play();
+        } else{
+            result = 'Incorrect' + inCorrectWav.play();
+        }
+        return result;
+        globalIndex++;
+        renderQuest();
+    }
+
     
 function renderQuest(){
-    var questionContainerEl = document.getElementById('questions');
-    var titleEl = document.createElement('h2')
-    titleEl = questions[0].title
+    for(var i = 0; i < 3; i++){
+        var questionContainerEl = document.getElementById('questions');
+        var titleEl = document.createElement('h2');
+        var option = document.createElement('button');
+        option.textContent = '1.option';
+        option.onclick = checkAnswer;
+
+        titleEl = questions[globalIndex].title;
+
+        questionContainerEl.appendChild(titleEl);
+        questionContainerEl.appendChild(option);
+    }
 }
 
 
@@ -22,14 +73,15 @@ function timerFunc(){
     }
 };
 
-create Elements
+// create Elements
 
-assign the elememts text
+// assign the elememts text
 
-if 
+// if 
 
 function init(){
     timerinterval=setInterval(timerfunc,1000);
+    renderQuest();
 }
 
 startBtnEl.onclick=init;
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,2 @@
 // TODO: store all questions here
 // NOTE: hint - can use an array that contains objects
-var questions = [ 
-    {
-        "title": "question1"
-        "options": ["opt1", "opt2", "opt3", "opt4"]
-        "answer": "opt1"
-    }
-
-    {
-        "title": "question2"
-        "options": ["opt1", "opt2", "opt3", "opt4"]
-        "answer": "opt3"
-    }
-
-    {
-        "title": "question3"
-        "options": ["opt1", "opt2", "opt3", "opt4"]
-        "answer": "opt4"
-    }
-
-    {
-        "title": "question4"
-        "options": ["opt1", "opt2", "opt3", "opt4"]
-        "answer": "opt2"
-    }
-]
